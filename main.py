579fc14f423b34305f0fc6448a697390d3b1b52d
ndjffkdm <makkonn25@gmail.com>
2024-12-24 00:20:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Hello, World")
+print(type("Hello, World"))
+
+print(34)
+print(type(34))
+
+print(34.5)
+print(type(34.5))
+
+print(34+468)
+print(36**8)
+#коменты
+# + - * /
+# // целочисленное деление
+# ** степень % остаток от деления
+
+print(3/5)
+print(3//5)
+
+print("hello " + str(3) + " world")
+
+#True False
+print(True)
+print(False)
+
+print(5>1)
+print(5<1)
+
+# > < == != (неравно) >= <=
+#and и or в лекции
